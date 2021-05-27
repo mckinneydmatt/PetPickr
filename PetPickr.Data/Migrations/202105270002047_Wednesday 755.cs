b55bf0d66706a4ca013c8011e6ae1d61475bfc71
mckinneydmatt <mckinneydmatt@gmail.com>
2021-05-27 13:32:52 -0400

    Fully deployed

---
+++ PetPickr.Data/Migrations/202105270002047_Wednesday 755.cs
@@ -0,0 +1,157 @@
+﻿namespace PetPickr.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Wednesday755 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Cat",
+                c => new
+                    {
+                        CatId = c.Int(nullable: false, identity: true),
+                        CatName = c.String(nullable: false),
+                        CatSex = c.Int(nullable: false),
+                        CatWeight = c.Int(nullable: false),
+                        CatAge = c.Int(nullable: false),
+                        CatPrice = c.Double(nullable: false),
+                        ShelterId = c.Int(nullable: false),
+                        CatImage = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.CatId)
+                .ForeignKey("dbo.Shelter", t => t.ShelterId, cascadeDelete: true)
+                .Index(t => t.ShelterId);
+            
+            CreateTable(
+                "dbo.Shelter",
+                c => new
+                    {
+                        ShelterId = c.Int(nullable: false, identity: true),
+                        ShelterName = c.String(nullable: false),
+                        ShelterAddress = c.String(nullable: false),
+                        ShelterPhoneNumber = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.ShelterId);
+            
+            CreateTable(
+                "dbo.Dog",
+                c => new
+                    {
+                        DogId = c.Int(nullable: false, identity: true),
+                        DogName = c.String(nullable: false),
+                        DogBreed = c.String(nullable: false),
+                        DogSex = c.Int(),
+                        DogWeight = c.Int(nullable: false),
+                        DogAge = c.Int(nullable: false),
+                        DogPrice = c.Double(nullable: false),
+                        ShelterId = c.Int(nullable: false),
+                        DogImage = c.String(),
+                    })
+                .PrimaryKey(t => t.DogId)
+                .ForeignKey("dbo.Shelter", t => t.ShelterId, cascadeDelete: true)
+                .Index(t => t.ShelterId);
+            
+            CreateTable(
+                "dbo.IdentityRole",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.IdentityUserRole",
+                c => new
+                    {
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        RoleId = c.String(),
+                        IdentityRole_Id = c.String(maxLength: 128),
+                        ApplicationUser_Id = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.UserId)
+                .ForeignKey("dbo.IdentityRole", t => t.IdentityRole_Id)
+                .ForeignKey("dbo.ApplicationUser", t => t.ApplicationUser_Id)
+                .Index(t => t.IdentityRole_Id)
+                .Index(t => t.ApplicationUser_Id);
+            
+            CreateTable(
+                "dbo.ApplicationUser",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        FullName = c.String(),
+                        FirstName = c.String(),
+                        LastName = c.String(),
+                        PhoneNumber = c.String(),
+                        Address = c.String(),
+                        Role = c.String(),
+                        Email = c.String(),
+                        EmailConfirmed = c.Boolean(nullable: false),
+                        PasswordHash = c.String(),
+                        SecurityStamp = c.String(),
+                        PhoneNumberConfirmed = c.Boolean(nullable: false),
+                        TwoFactorEnabled = c.Boolean(nullable: false),
+                        LockoutEndDateUtc = c.DateTime(),
+                        LockoutEnabled = c.Boolean(nullable: false),
+                        AccessFailedCount = c.Int(nullable: false),
+                        UserName = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.IdentityUserClaim",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.String(),
+                        ClaimType = c.String(),
+                        ClaimValue = c.String(),
+                        ApplicationUser_Id = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.ApplicationUser", t => t.ApplicationUser_Id)
+                .Index(t => t.ApplicationUser_Id);
+            
+            CreateTable(
+                "dbo.IdentityUserLogin",
+                c => new
+                    {
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        LoginProvider = c.String(),
+                        ProviderKey = c.String(),
+                        ApplicationUser_Id = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.UserId)
+                .ForeignKey("dbo.ApplicationUser", t => t.ApplicationUser_Id)
+                .Index(t => t.ApplicationUser_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.IdentityUserRole", "ApplicationUser_Id", "dbo.ApplicationUser");
+            DropForeignKey("dbo.IdentityUserLogin", "ApplicationUser_Id", "dbo.ApplicationUser");
+            DropForeignKey("dbo.IdentityUserClaim", "ApplicationUser_Id", "dbo.ApplicationUser");
+            DropForeignKey("dbo.IdentityUserRole", "IdentityRole_Id", "dbo.IdentityRole");
+            DropForeignKey("dbo.Cat", "ShelterId", "dbo.Shelter");
+            DropForeignKey("dbo.Dog", "ShelterId", "dbo.Shelter");
+            DropIndex("dbo.IdentityUserLogin", new[] { "ApplicationUser_Id" });
+            DropIndex("dbo.IdentityUserClaim", new[] { "ApplicationUser_Id" });
+            DropIndex("dbo.IdentityUserRole", new[] { "ApplicationUser_Id" });
+            DropIndex("dbo.IdentityUserRole", new[] { "IdentityRole_Id" });
+            DropIndex("dbo.Dog", new[] { "ShelterId" });
+            DropIndex("dbo.Cat", new[] { "ShelterId" });
+            DropTable("dbo.IdentityUserLogin");
+            DropTable("dbo.IdentityUserClaim");
+            DropTable("dbo.ApplicationUser");
+            DropTable("dbo.IdentityUserRole");
+            DropTable("dbo.IdentityRole");
+            DropTable("dbo.Dog");
+            DropTable("dbo.Shelter");
+            DropTable("dbo.Cat");
+        }
+    }
+}
--- PetPickr.Data/Migrations/Configuration.cs
+++ PetPickr.Data/Migrations/Configuration.cs
@@ -1,10 +1,9 @@
-namespace PetPickr.Data.Migrations
+﻿namespace PetPickr.Data.Migrations
 {
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
-    using PetPickr.Data;
 
     internal sealed class Configuration : DbMigrationsConfiguration<PetPickr.Data.ApplicationDbContext>
     {
@@ -14,88 +13,23 @@ namespace PetPickr.Data.Migrations
         }
 
         protected override void Seed(PetPickr.Data.ApplicationDbContext context)
-        { 
+        {
             context.Shelters.AddOrUpdate(s => s.ShelterId,
             new Shelter() { ShelterId = 4, ShelterName = "Indy Animal Services", ShelterAddress = "1330 N Meridian St, Indianapolis, IN 46202", ShelterPhoneNumber = "317-555-5139" },
             new Shelter() { ShelterId = 5, ShelterName = "Central Indiana Humane Society", ShelterAddress = "50 W. Ohio St, Indianapolis, IN 46202", ShelterPhoneNumber = "317-555-1344" }
-            ); 
-        
+            );
+
             context.Dogs.AddOrUpdate(d => d.DogId,
             new Dog() { DogId = 10, DogName = "Roxy", DogBreed = "Italian Greyhound", DogSex = DogSex.Female, DogWeight = 15, DogAge = 4, DogPrice = 100, ShelterId = 4, DogImage = "https://www.pixelsbin.com/images/2021/05/26/Roxy922614384e661b35.jpg" },
             new Dog() { DogId = 11, DogName = "Ira", DogBreed = "Border Collie/Mutt", DogSex = DogSex.Male, DogWeight = 30, DogAge = 6, DogPrice = 75, ShelterId = 5, DogImage = "https://www.pixelsbin.com/images/2021/05/26/Ira94351549ac81f610.jpg" },
             new Dog() { DogId = 12, DogName = "Reggie", DogBreed = "Golden Mix", DogSex = DogSex.Male, DogWeight = 10, DogAge = 1, DogPrice = 125, ShelterId = 4, DogImage = "https://www.pixelsbin.com/images/2021/05/26/Reggie185ae0a5fbac9b8c.jpg" },
             new Dog() { DogId = 13, DogName = "Maci", DogBreed = "Golden Retreiver", DogSex = DogSex.Female, DogWeight = 40, DogAge = 11, DogPrice = 50, ShelterId = 4, DogImage = "https://www.pixelsbin.com/images/2021/05/26/Maci5b96097669569e59.jpg" }
-            ); 
+            );
 
-           context.Cats.AddOrUpdate(c => c.CatId,
-            new Cat() { CatId = 21, CatName = "Sophie", CatSex = CatSex.Female, CatWeight = 15, CatAge = 4, CatPrice = 25, ShelterId = 5, CatImage = "https://www.pixelsbin.com/images/2021/05/26/Sophieedd55482a3fff16c.jpg" },
-             new Cat() { CatId = 22, CatName = "Olive", CatSex = CatSex.Female, CatWeight = 10, CatAge = 2, CatPrice = 35, ShelterId = 5, CatImage = "https://www.pixelsbin.com/images/2021/05/26/Oliveb7ac2e3f28d1dec0.jpg" }
-             );
+            context.Cats.AddOrUpdate(c => c.CatId,
+             new Cat() { CatId = 21, CatName = "Sophie", CatSex = CatSex.Female, CatWeight = 15, CatAge = 4, CatPrice = 25, ShelterId = 5, CatImage = "https://www.pixelsbin.com/images/2021/05/26/Sophieedd55482a3fff16c.jpg" },
+              new Cat() { CatId = 22, CatName = "Olive", CatSex = CatSex.Female, CatWeight = 10, CatAge = 2, CatPrice = 35, ShelterId = 5, CatImage = "https://www.pixelsbin.com/images/2021/05/26/Oliveb7ac2e3f28d1dec0.jpg" }
+              );
         }
-        //    context.Dogs.AddOrUpdate(d => d.DogId,
-        //        new Dog() { DogId = 1, DogName = "Roxy", DogBreed = "Italian Greyhound", DogSex=2, DogWeight = 15, DogAge=4, DogPrice=100,ShelterId= }
-        //        )
-
-
-        //{
-        //    context.Authors.AddOrUpdate(x => x.Id,
-        //        new Author() { Id = 1, Name = "Jane Austen" },
-        //        new Author() { Id = 2, Name = "Charles Dickens" },
-        //        new Author() { Id = 3, Name = "Miguel de Cervantes" }
-        //        );
-
-        //    context.Books.AddOrUpdate(x => x.Id,
-        //        new Book()
-        //        {
-        //            Id = 1,
-        //            Title = "Pride and Prejudice",
-        //            Year = 1813,
-        //            AuthorId = 1,
-        //            Price = 9.99M,
-        //            Genre = "Comedy of manners"
-        //        },
-        //        new Book()
-        //        {
-        //            Id = 2,
-        //            Title = "Northanger Abbey",
-        //            Year = 1817,
-        //            AuthorId = 1,
-        //            Price = 12.95M,
-        //            Genre = "Gothic parody"
-        //        },
-        //        new Book()
-        //        {
-        //            Id = 3,
-        //            Title = "David Copperfield",
-        //            Year = 1850,
-        //            AuthorId = 2,
-        //            Price = 15,
-        //            Genre = "Bildungsroman"
-        //        },
-        //        new Book()
-        //        {
-        //            Id = 4,
-        //            Title = "Don Quixote",
-        //            Year = 1617,
-        //            AuthorId = 3,
-        //            Price = 8.95M,
-        //            Genre = "Picaresque"
-        //        }
-        //        );
-        //}
-
-        //  This method will be called after migrating to the latest version.
-
-        //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-        //  to avoid creating duplicate seed data. E.g.
-        //
-        //    context.People.AddOrUpdate(
-        //      p => p.FullName,
-        //      new Person { FullName = "Andrew Peters" },
-        //      new Person { FullName = "Brice Lambson" },
-        //      new Person { FullName = "Rowan Miller" }
-        //    );
-        //
     }
 }
-
